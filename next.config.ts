00f889ac01ed148d14a64683aaa0df8d9cb071ac
Collins <officialrrye5@gmail.com>
2025-01-20 07:46:06 +0100

    More Error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    reactStrictMode: true,
+    // This ensures the .well-known directory is accessible
+    async headers() {
+        return [
+            {
+                source: '/.well-known/assetlinks.json',
+                headers: [
+                    {
+                        key: 'Content-Type',
+                        value: 'application/json',
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
