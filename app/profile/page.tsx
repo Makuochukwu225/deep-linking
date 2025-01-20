e7ba5d9820d56e4b61cde681acda986b839a9804
Collins <officialrrye5@gmail.com>
2025-01-20 07:54:25 +0100

    More Error

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,25 +1,23 @@
-export default function Home() {
+export default function Profile() {
     return (
         <div className="p-8">
-            <h1>Deep Linking Test</h1>
+            <h1>Profile Page</h1>
+            <p>This is the deep linked profile page</p>
+
+            {/* Add buttons to test deep linking */}
             <div className="mt-4 space-y-4">
                 <a
                     href="kolnapp://profile?id=123"
-                    className="text-blue-500 underline block"
+                    className="text-blue-500 underline"
                 >
                     Open in App (URL Scheme)
                 </a>
+                <br/>
                 <a
                     href="https://deep-linking-bice.vercel.app/profile?id=123"
-                    className="text-blue-500 underline block"
+                    className="text-blue-500 underline"
                 >
                     Open in App (Universal Link)
-                </a>
-                <a
-                    href="/profile?id=123"
-                    className="text-blue-500 underline block"
-                >
-                    Open Profile Page
                 </a>
             </div>
         </div>
